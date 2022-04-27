2b63b6b85fb8ff9e496ebc35cd69afbc01e38be8
miao202 <95266076+miao202@users.noreply.github.com>
2022-04-27 17:43:47 +0800

    Update domain_adaptation.py

--- reid/datasets/domain_adaptation.py
+++ reid/datasets/domain_adaptation.py
@@ -15,23 +15,27 @@ import math
 class DA(object):
 
     def __init__(self, data_dir, source, target):
-
+       #osp.join用于拼接文件路径，找到对应的路径赋值
         self.source_images_dir = osp.join(data_dir, source)
         self.target_images_dir = osp.join(data_dir, target)
+       #找到三类数据的路径
         self.source_train_path = 'bounding_box_train'
         self.gallery_path = 'bounding_box_test'
         self.query_path = 'query'
-
+       #赋初始值
         self.source_train, self.query, self.gallery = [], [], []
         self.num_train_ids, self.num_query_ids, self.num_gallery_ids, self.num_generate_ids = 0, 0, 0, 0
-        self.source_num_cam = 6 if 'market' in source else 8
-        self.pid_num = 0
+        self.source_num_cam = 6 if 'market' in source else 8 #cam相机数目
+        self.pid_num = 0  #行人id数目
         self.load()
     
     def preprocess(self, images_dir, path, relabel=True):
         self.pid_num = 0
+       #将正则表达式字符串编译成pattern对象
+       #通过pattern对象提供的文本匹配查找并获得匹配结果
         pattern = re.compile(r'([-\d]+)_c?(\d+)(\w+)(?:-(\d+))?\.jpg')
-
+       #查找符合图像，用fpaths进行for循环可以使数据更紧致
+       #循环遍历所有图像
         all_pids = {}
         ret = []
         fpaths = sorted(glob(osp.join(images_dir, path, '*.jpg')))
@@ -59,7 +63,7 @@ class DA(object):
             if pid > self.pid_num: self.pid_num = pid
             cam -= 1
             
-            # add image:
+            # 增加图片
             img_pil= Image.open(fname)
             resize = Resize(
             size=(256, 128),  # (height, width)
@@ -68,7 +72,7 @@ class DA(object):
             ret.append((img_pil, fname, pid, cam, pindex))
 
         return ret, int(len(all_pids)), len(ret)
-
+    #将读取数据信息输出
     def load(self):
         self.source_train, self.num_train_ids, self.source_pindex = self.preprocess(self.source_images_dir, self.source_train_path)
         self.gallery, self.num_gallery_ids, _ = self.preprocess(self.target_images_dir, self.gallery_path, False)
